8aa40536073ddb497867866009389f244dc10eab
Đỗ Thành <126317114+thanhdo1110@users.noreply.github.com>
2024-12-01 17:06:05 +0700

    v1.1
    Đổi từ Zone1110 - > Apikey1110 và fix 1 số cái nữa

---
+++ Zone1110/Apikey1110.h
@@ -0,0 +1,17 @@
+#import <Foundation/Foundation.h>
+//#import <UIKit/UIKit.h>
+
+@interface Apikey1110 : NSObject
+NS_ASSUME_NONNULL_BEGIN
+- (void)setToken:(NSString *)token;
+- (void)paid:(void (^)(void))execute;
+- (NSString *)Timekey;
+- (NSString *)deviceType1110;
+- (NSString *)iOSVersion;
+- (NSString *)getUDID;
+- (NSString *)NameApp;
+- (NSString *)checkJailbreakStatus;
+extern NSString *const __contact;
+extern NSString *const __Confirm;
+@end
+NS_ASSUME_NONNULL_END
--- Zone1110/hash.mm
+++ Zone1110/hash.mm
@@ -1,5 +1,6 @@
-#import "Zone1110.h"
+#import "Apikey1110.h"
 #import "encrypt.h"
 #import <Foundation/Foundation.h>
 NSString *const __contact = NSSENCRYPT("Liên hệ"); // Nội dung nút liên hệ
 NSString *const __Confirm = NSSENCRYPT("Xác nhận"); // Nội dung nút xác nhận
+                             
